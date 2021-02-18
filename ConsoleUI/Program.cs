14ec0ae5073ae135cd15bd143e018c3dd0354fe6
İsmail Sülkü <ismailsulku@gmail.com>
2021-02-18 21:10:43 +0300

    18.02.2021

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -24,6 +24,7 @@ namespace ConsoleUI
             foreach (var category in categoryManager.GetAll())
             {
                 Console.WriteLine(category.CategoryName);
+
             }
         }
 
